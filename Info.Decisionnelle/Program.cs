5297e6df0c39043482a943698724398a735f96ce
Clara Elkreidy <clarakreidy@hotmail.com>
2022-02-08 16:12:14 +0100

    initializing repository

---
+++ Info.Decisionnelle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Info.Decisionnelle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
